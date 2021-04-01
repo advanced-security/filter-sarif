ffc5cf45fdd0758577fb9cc51d3a9c95cbd39ceb
Sebastian Bauersfeld <sebastian@semmle.com>
2021-04-01 13:23:13 +0200

    update

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM ubuntu:latest
-RUN apt-get update && apt-get install -y python3
-COPY entrypoint.sh /entrypoint.sh
-COPY filter-sarif *.py /
-ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-unset LD_PRELOAD
-/filter-sarif \
-  --input "/github/workspace/${INPUT_INPUT}" \
-  --output "/github/workspace/${INPUT_OUTPUT}" \
-  --split-lines \
-  -- \
-  "$INPUT_PATTERNS"
